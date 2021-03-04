ee6c12ca58aa38c9aa329e7a48568448285288f7
bnc464464 <bnc464464@gmail.com>
2021-03-05 08:54:17 +1300

    Add project files.

---
+++ MMF_Base_V1/Program.cs
@@ -0,0 +1,18 @@
+﻿//using statements
+using System;
+
+namespace MMF_Base_V1
+{
+    class Program
+    {
+        //functions go here
+
+
+
+        //++++++++++++ Main Routine ++++++++++++
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
