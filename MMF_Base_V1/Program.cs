c448b0ef7f5ddf77d2f326d4d4cdea1fc56e68c4
bnc464464 <bnc464464@gmail.com>
2021-03-05 09:06:31 +1300

    All the comments

--- MMF_Base_V1/Program.cs
+++ MMF_Base_V1/Program.cs
@@ -5,14 +5,36 @@ namespace MMF_Base_V1
 {
     class Program
     {
-        //functions go here
+        //Set up dictionaries / lists needed to hold data
+
+
+
+        //Functions go here
 
 
 
         //++++++++++++ Main Routine ++++++++++++
         static void Main(string[] args)
         {
-        
+            //Ask user if they have used the program before & show instructions if necessary
+
+                //Loop to get ticket details
+
+                // Get name (Not blank)
+
+                //Get age (Between 12 and 130)
+
+                //Calculate ticket price
+
+                //Loop to ask for snacks
+
+                //Calculate snack price
+
+                //Ask for payment method (And apply surcharge if necesary
+
+            //Calculate total sales and profit
+
+            //Output data to text file
         }
     }
 }
